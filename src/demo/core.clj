3195176b27f90c33c12764dc77c3997b70602a74
Carlos Monteiro <carlosem4il@gmail.com>
2025-05-16 01:33:10 -0300

    adding more functions

--- src/demo/core.clj
+++ src/demo/core.clj
@@ -84,6 +84,79 @@
   (let [[first-value & rest-of-values] my-map]
     (str first-value " ||| " rest-of-values)))
 
+; 11
+; You can use map with multiples collections.
+;  (map str ["a" "b" "c"] ["A" "B" "C"])
+;  ; => ("aA" "bB" "cC")
+; It’s as if map does the following:
+; (list (str "a" "A") (str "b" "B") (str "c" "C"))
+;
+; Just be sure that your mapping
+; function can take a number of arguments equal to the number of collections you’re passing to map.
+(defn using-map-with-multiple-lists
+  [l1 l2]
+  (map str l1 l2))
+
+; 12
+; You can use a map to retrieve the value associated with a keyword from a collection of map.
+; Because keywords can be used as functions, you can do this succinctly.
+(defn extract-keys-of-list-or-vector-of-maps
+  []
+  (let [vector-of-maps [{:name "Bob" :age 20} {:name "Lucas" :age 18} {:name "Willian" :age 30}]]
+        (map :name vector-of-maps)))
+
+; 13 - take, drop
+; take and drop both take two arguments: a number and a sequence. take
+; returns the first n elements of the sequence, whereas drop returns the
+;sequence with the first n elements removed
+; (take 3 [1 2 3 4 5 6 7 8 9 10])
+; => (1 2 3)
+; (drop 3 [1 2 3 4 5 6 7 8 9 10])
+; => (4 5 6 7 8 9 10)
+
+; 14 - take-while, and drop-while
+; Each takes a predicate function (a function whose return value is evaluated
+;for truth or falsity) to determine when it should stop taking or dropping.
+;
+; (take-while #(< (:month %) 3) food-journal)
+
+; 15 - filter and some
+; Use filter to return all elements of a sequence that test true for a predicate
+;function.
+; (filter #(< (:human %) 5) food-journal)
+;
+; The some function does that, returning
+;the first truthy value (any value that’s not false or nil) returned by a predi-
+;cate function:
+;(some #(> (:critter %)
+;
+; (some #(> (:critter %) 5) food-journal)
+;; => nil
+;(some #(> (:critter %) 3) food-journal)
+;; => true
+
+; 16 - sort and sort-by
+;
+; You can sort elements in ascending order with sort:
+;(sort [3 1 2])
+;; => (1 2 3)
+;
+; If your sorting needs are more complicated, you can use sort-by, which
+;allows you to apply a function (sometimes called a key function) to the ele-
+;ments of a sequence and use the values it returns to determine the sort
+;order.
+;
+; (sort-by count ["aaa" "c" "bb"])
+;; => ("c" "bb" "aaa")
+
+
+; 17 - concat
+;
+; Finally, concat simply appends the members of one sequence to the end of
+;another:
+; (concat [1 2] [3 4])
+;; => (1 2 3 4)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
@@ -98,5 +171,7 @@
   (println "7) After add an element to my list | Before " my-list " | After " (add-element-to-list my-list) " | Now " my-list)
   (println "8)" (get-first-element-from-vector my-vector))
   (println "9) After add an element to my vector | Before " my-vector " | After " (add-element-to-list my-vector) " | Now " my-vector)
-  (println "10) Destructing a map wih let: " (destructing-with-let)))
-
+  (println "10) Destructing a map wih let: " (destructing-with-let))
+  (println "11) Map with multiples inputs, (a b c) and (1 2 3) equal to:" (using-map-with-multiple-lists '("a" "b" "c") '(1 2 3)))
+  (println "12) Extracting keys from vector or list of maps using map function: " (extract-keys-of-list-or-vector-of-maps))
+  )
